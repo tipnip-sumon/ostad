03528da2804663c050a05c12a5d7f396c2927a18
tipnip-sumon <sumonmti498@gmail.com>
2025-05-06 00:15:34 +0600

    Inheritence

--- lib/inheritence.dart
+++ lib/inheritence.dart
@@ -1,17 +1,19 @@
 class Father{
   String name;
+  int age;
   String land = '10 bigha';
   String house = 'Tin er ghor';
 
-  Father(this.name){
-    print("$name object created");
+  Father(this.name,this.age){
+    print("$name $age object created");
   }
 }
 class Son extends Father{
-  Son():super('Abu Md Eyahia');
+  Son():super('Abu Md Eyahia',10);
 }
 void main(){
   Son name = Son();
   print(name.land);
-  Father fth = Father('Abu Md Eyahia');
+  Father fth = Father('Abu Md Eyahia',100);
+  print(fth.age);
 }
